48f2be661decdec611547028d7e222260cda2f29
panchicore <luis@pallares.co>
2011-10-19 18:31:11 -0500

    admin site in root path

--- cdr/views.py
+++ cdr/views.py
@@ -1,1 +1,5 @@
 # Create your views here.
+from django.http import HttpResponseRedirect
+
+def home(request):
+    return HttpResponseRedirect('/admin/')
--- urls.py
+++ urls.py
@@ -6,12 +6,12 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'asteriskcdrdb.views.home', name='home'),
+    url(r'^$', 'cdr.views.home', name='home'),
     # url(r'^asteriskcdrdb/', include('asteriskcdrdb.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    url(r'^$', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
