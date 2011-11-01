3d28fbe43baed2cf582c5bd8e32c58cfccf8584b
panchicore <luis@pallares.co>
2011-10-31 19:34:09 -0500

    add jquery

--- cdr/views.py
+++ cdr/views.py
@@ -3,9 +3,11 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.template.context import RequestContext
 from cdr.models import Cdr
+from datetime import date, datetime
 
 def home(request):
-    registros = Cdr.objects.all().using(request.database)[0:30]
+    TODAY = date.today()
+    registros = Cdr.objects.filter(calldate=TODAY).using(request.database)
     return render_to_response("cdr/index.html",
                                 {'registros':registros},
                                 context_instance=RequestContext(request))
